8b209c4db9f5c51c525d309edfa31fede3990ceb
Arisch24 <arischvaran00@gmail.com>
2022-04-15 23:59:35 +0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,150 @@
+// Load an external vue component
+function load(comp) {
+    return resolve => fetch(`_${comp}.html`)
+        .then(resp => resp.text())
+        .then(text => { 
+            $(document.body).append(text);
+            resolve(Vue.component(comp));
+        });
+}
+
+// Auto focus on a given input field when component loaded
+Vue.directive('focus', {
+    inserted(el) {
+        el.focus();
+    }
+});
+
+// Format a number to the given decimal places
+Vue.filter('number', (value, decimal = 2) => {
+    return parseFloat(value).toFixed(decimal);
+});
+
+// Format gender to its relevant emoji (👩🏻‍🎓 or 👨🏻‍🎓) and full word
+Vue.filter('gender', value => {
+    return value == 'F' ? '👩🏻‍🎓 Female' : '👨🏻‍🎓 Male';
+});
+
+// Format fb profile id to fb profile photo url
+Vue.filter('fb', value => {
+    return `https://graph.facebook.com/${value}/picture?width=100&height=100`;
+});
+
+// PURPOSE: Center-crop image to the width and height specified (upscale)
+function crop(f, w, h, to = 'blob', type = 'image/jpeg') {
+    return new Promise((resolve, reject) => { 
+        const img = document.createElement('img');
+        
+        img.onload = e => {
+            URL.revokeObjectURL(img.src);
+            
+            // Resize algorithm ---------------------------
+            let ratio = w / h;
+
+            let nw = img.naturalWidth;
+            let nh = img.naturalHeight;
+            let nratio = nw / nh;
+
+            let sx, sy, sw, sh;
+
+            if (ratio >= nratio) {
+                // Retain width, calculate height
+                sw = nw;
+                sh = nw / ratio;
+                sx = 0;
+                sy = (nh - sh) / 2;
+            }
+            else {
+                // Retain height, calculate width
+                sw = nh * ratio;
+                sh = nh;
+                sx = (nw - sw) / 2;
+                sy = 0;
+            }
+            // --------------------------------------------
+
+            const can = document.createElement('canvas');
+            can.width  = w;
+            can.height = h;
+            can.getContext('2d').drawImage(img, sx, sy, sw, sh, 0, 0, w, h);
+
+            // Resolve to blob or dataURL
+            if (to == 'blob') {
+                can.toBlob(blob => resolve(blob), type);
+            }
+            else if (to == 'dataURL') {
+                let dataURL = can.toDataURL(type);
+                resolve(dataURL);
+            }
+            else {
+                reject('ERROR: Specify blob or dataURL');
+            }
+        };
+
+        img.onerror = e => {
+            URL.revokeObjectURL(img.src);
+            reject('ERROR: File is not an image');
+        };
+
+        img.src = URL.createObjectURL(f);
+    });
+}
+
+// PURPOSE: Best-fit image within the width and height specified (no upscale)
+function fit(f, w, h, to = 'blob', type = 'image/jpeg') {
+    return new Promise((resolve, reject) => { 
+        const img = document.createElement('img');
+        
+        img.onload = e => {
+            URL.revokeObjectURL(img.src);
+            
+            // Resize algorithm ---------------------------
+            let ratio = w / h;
+
+            let nw = img.naturalWidth;
+            let nh = img.naturalHeight;
+            let nratio = nw / nh;
+
+            if (nw <= w && nh <= h) {
+                // Smaller than targetted width and height, do nothing
+                w = nw;
+                h = nh;
+            }
+            else {
+                if (nratio >= ratio) {
+                    // Retain width, calculate height
+                    h = w / nratio;
+                }
+                else {
+                    // Retain height, calculate width
+                    w = h * nratio;
+                }
+            }
+            // --------------------------------------------
+
+            const can = document.createElement('canvas');
+            can.width  = w;
+            can.height = h;
+            can.getContext('2d').drawImage(img, 0, 0, w, h);
+
+            // Resolve
+            if (to == 'blob') {
+                can.toBlob(blob => resolve(blob), type);
+            }
+            else if (to == 'dataURL') {
+                let dataURL = can.toDataURL(type);
+                resolve(dataURL);
+            }
+            else {
+                reject('ERROR: Specify blob or dataURL');
+            }
+        };
+
+        img.onerror = e => {
+            URL.revokeObjectURL(img.src);
+            reject('ERROR: File is not an image');
+        };
+
+        img.src = URL.createObjectURL(f);
+    });
+}
